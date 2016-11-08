326e27ddd23864b56d35c5b8a3827352c65107fb
Tomasz <tete10@wp.pl>
2016-11-08 15:58:12 +0100

    clean some files ;)

--- templates/nav.html_conflict-20161030-183842.php
+++
@@ -1,7 +0,0 @@
-<div id="navi" class="w3-container">
-<ul class="w3-navbar w3-blue-grey">
-	<li><a href="index.php?task=categories&action=index">Lista kategorii</a></li>
-    <li><a href="?task=categories&action=add">Dodaj kategorię</a></li>
-	<li><a href="info/info.html.php">O aplikacji</a><li>
-</ul>
-</div>
